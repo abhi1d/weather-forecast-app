4fcf278c1cd54bfcec566916a966cd29d37b2a28
Abhishek Sharma <Abhishek.Sharma@Yodlee.com>
2019-08-21 19:40:49 +0530

    search bar is ready for weather forecast

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+console.log('Client side javascript file is loaded')
+
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = 'from Loding...'
+messageTwo.textContent = ''
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = searchElement.value
+
+    console.log(location)
+    fetch('http://localhost:3000/weather?address='+location+'').then((response) => {
+
+        response.json().then((data) => {
+        if(data.error){
+           return messageOne.textContent = data.error
+        }
+
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.temperature
+
+        })
+    })
+})
